20a21d8f6a50c82aa7930951e8bc9f4ee0292aa7
yonka <252941898@qq.com>
2016-04-11 09:36:45 +0800

    improve autp_upload_ip_info

--- temp_snippets/tools/op/network/auto_upload_ip_info.py
+++ temp_snippets/tools/op/network/auto_upload_ip_info.py
@@ -4,20 +4,26 @@ import json
 import os
 import sys
 import optparse
+from os.path import expanduser
+import time
+import ipaddress
 
-import crypto
-
-
-sys.modules["Crypto"] = crypto
+if os.name == "nt":
+    import crypto
+    sys.modules["Crypto"] = crypto
 import paramiko
 import netifaces
 
 __author__ = 'fzh89'
 
 g_action = None
+g_last_ip_info = None
 
 
 def get_home_dir_path() -> str:
+    """
+    ~形式的不能在代码里cd，要展开
+    """
     def _win_home() -> str:
         home_drive = os.environ.get("HOMEDRIVE")
         home_path = os.environ.get("HOMEPATH")
@@ -35,7 +41,7 @@ def get_home_dir_path() -> str:
     return os_home_dir_path_getter()
 
 
-def get_ip_info():
+def get_ip_info() -> dict:
     ip_infos = {}
     for ifaceName in netifaces.interfaces():
         addresses = [i['addr'] for i in netifaces.ifaddresses(ifaceName).setdefault(
@@ -47,7 +53,7 @@ def get_ip_info():
 def prepare_client() -> paramiko.SSHClient:
     host = "yonka-linode-vps-1"  # bobo-aliyun-xueshengji
     username = "raspberry"
-    home_dir_path = get_home_dir_path()
+    home_dir_path = expanduser("~")
     if not home_dir_path:
         raise SystemError("can not get home dir path")
     private_key_file = os.path.join(home_dir_path, ".ssh", "id_rsa")
@@ -60,8 +66,23 @@ def prepare_client() -> paramiko.SSHClient:
     return ssh
 
 
-def upload():
-    ip_info = get_ip_info()
+def may_online(ip_info) -> bool:
+    if ip_info is None:
+        return False
+    for ifname, ifaddr in ip_info.items():
+        if not ifaddr:
+            continue
+        for addr in ifaddr:
+            if not addr:
+                continue
+            if not ipaddress.ip_address(addr).is_private:
+                return True
+    return False
+
+
+def upload(ip_info: dict=None):
+    if ip_info is None:
+        ip_info = get_ip_info()
     info_to_send = {
         "time": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
         "ip_info": ip_info
@@ -85,6 +106,46 @@ def get():
     ssh.close()
 
 
+def cmp_addr_info(last_one, new_one):
+    if last_one is None:
+        if new_one is None:
+            return True
+        else:
+            return False
+    if new_one is None:
+        return False
+    if len(last_one) != len(new_one):
+        return False
+    for last_addr, new_addr in zip(last_one, new_one):
+        if last_addr != new_addr:
+            return False
+    return True
+
+
+def cmp_ip_info(last_one, new_one):
+    if last_one is None:
+        if new_one is not None:
+            return False
+    if new_one is None:
+        return True
+    if len(last_one) != len(new_one):
+        return False
+    for ifname, ifaddr in last_one.items():
+        if not cmp_addr_info(ifaddr, new_one.get(ifname)):
+            return False
+    return True
+
+
+def monitor(intervals=5):
+    global g_last_ip_info
+    while True:
+        time.sleep(intervals)
+        ip_info = get_ip_info()
+        if ip_info is not None and not cmp_ip_info(g_last_ip_info, ip_info) and may_online(ip_info):
+            upload(ip_info)
+            g_last_ip_info = ip_info
+
+
 def main(a):
     globals()[a]()
 
@@ -95,6 +156,8 @@ if __name__ == "__main__":
     python auto_upload_ip_info.py --action=upload
     or
     python auto_upload_ip_info.py --action=get
+    or
+    python auto_upload_ip_info.py --action=monitor
     \n
     """
     g_parser = optparse.OptionParser(g_help_info)
@@ -102,7 +165,7 @@ if __name__ == "__main__":
         "-a",
         "--action",
         dest="a",
-        help="the action todo, upload ot get",
+        help="the action todo, upload ot get or monitors",
         default="get",
         action="store"
     )
